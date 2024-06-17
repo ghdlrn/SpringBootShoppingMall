289bb2baac0ecf4415e8e4f61479353a07451c31
ghdlrn <ghdlrn@gmail.com>
2024-06-17 13:03:55 +0900

    Changes

--- src/main/java/com/shop/repository/OrderItemRepository.java
+++ src/main/java/com/shop/repository/OrderItemRepository.java
@@ -1,7 +1,13 @@
 package com.shop.repository;
 
+import com.shop.dto.CartDetailDto;
 import com.shop.entity.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+
+
 }
